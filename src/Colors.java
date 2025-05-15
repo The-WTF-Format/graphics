263418558c57055f80c46d9932c2d90be3dc6ee2
Miriam Lazzaretti <miriam.lazzaretti@protonmail.com>
2025-05-15 14:20:22 +0200

    Color adjustments

--- src/Colors.java
+++ src/Colors.java
@@ -1,10 +1,10 @@
 import java.awt.*;
 
 public final class Colors {
-    public static final Color BACKGROUND = new Color(100, 100, 100);
-    public static final Color MENUBARBACKGROUND = new Color(130, 125, 125);
+    public static final Color BACKGROUND = new Color(110, 100, 100);
+    public static final Color MENUBARBACKGROUND = new Color(125, 125, 135);
     public static final Color ITEMSPRIMARY = new Color(170, 170, 170);
-    public static final Color ITEMSSECONDARY = new Color(185, 170, 170);
+    public static final Color ITEMSSECONDARY = new Color(175, 175, 175);
     private Colors() {
 
     }
